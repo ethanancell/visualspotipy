869aa4726d1f825eb1384a2563669f2846fdc27b
MusicDev33 <smccowan33@gmail.com>
2020-01-12 11:28:36 -0700

    Quick cleanup

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import spotipy
 import spotipy.util as util
 
 import os
+import sys
 import time
 from time import sleep
 
@@ -25,56 +26,56 @@ analysis_id_list = [analysis_id]
 # Get user credentials
 token = util.prompt_for_user_token(username, scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-
-    # Perform analysis
-    analysis = sp.audio_analysis(analysis_id)
-    analysis_sections = analysis['sections']
-    analysis_bars = analysis['bars']
-    analysis_beats = analysis['beats']
-
-    # Get track information
-    analysis_track = sp.track(analysis_id)
-
-    # Start the playback
-    sp.start_playback(uris=analysis_id_list)
-
-    # Start of the song to measure how far in we are
-    start_of_song = time.time()
-    length_of_song = analysis_track['duration_ms'] / 1000
-
-    # Set up the beats, sections, and bars
-    beat_unplayed = [True] * len(analysis_beats)
-    section_unplayed = [True] * len(analysis_sections)
-    bar_unplayed = [True] * len(analysis_bars)
-    current_beat = 0
-    current_section = 0
-    current_bar = 0
-
-    # Loop through the song
-    current_time = time.time() - start_of_song
-    while current_time < length_of_song:
-        current_time = time.time() - start_of_song
-        # Print a section
-        if section_unplayed[current_section] and analysis_sections[current_section + 1]['start'] > current_time > \
-                analysis_sections[current_section]['start']:
-            section_unplayed[current_section] = False
-            current_section += 1
-            print(
-                f"\nSection {current_section} \t Time Signature {analysis_sections[current_section - 1]['time_signature']}")
-
-        current_time = time.time() - start_of_song
-        # Print a beat
-        if beat_unplayed[current_beat] and analysis_beats[current_beat + 1]['start'] > current_time > \
-                analysis_beats[current_beat]['start']:
-            beat_unplayed[current_beat] = False
-            current_beat += 1
-            if analysis_beats[current_beat - 1]['confidence'] > 0.4:
-                print(f"Beat {current_beat}")
-
-        # Set what the current time in the song is
-        current_time = time.time() - start_of_song
-
-else:
+if not token:
     print("Can't get a valid user token for ", username)
+    sys.exit()
+
+sp = spotipy.Spotify(auth=token)
+
+# Perform analysis
+analysis = sp.audio_analysis(analysis_id)
+analysis_sections = analysis['sections']
+analysis_bars = analysis['bars']
+analysis_beats = analysis['beats']
+
+# Get track information
+analysis_track = sp.track(analysis_id)
+
+# Start the playback
+sp.start_playback(uris=analysis_id_list)
+
+# Start of the song to measure how far in we are
+start_of_song = time.time()
+length_of_song = analysis_track['duration_ms'] / 1000
+
+# Set up the beats, sections, and bars
+beat_unplayed = [True] * len(analysis_beats)
+section_unplayed = [True] * len(analysis_sections)
+bar_unplayed = [True] * len(analysis_bars)
+current_beat = 0
+current_section = 0
+current_bar = 0
+
+# Loop through the song
+current_time = time.time() - start_of_song
+while current_time < length_of_song:
+    current_time = time.time() - start_of_song
+    # Print a section
+    if section_unplayed[current_section] and analysis_sections[current_section + 1]['start'] > current_time > \
+            analysis_sections[current_section]['start']:
+        section_unplayed[current_section] = False
+        current_section += 1
+        print(
+            f"\nSection {current_section} \t Time Signature {analysis_sections[current_section - 1]['time_signature']}")
+
+    current_time = time.time() - start_of_song
+    # Print a beat
+    if beat_unplayed[current_beat] and analysis_beats[current_beat + 1]['start'] > current_time > \
+            analysis_beats[current_beat]['start']:
+        beat_unplayed[current_beat] = False
+        current_beat += 1
+        if analysis_beats[current_beat - 1]['confidence'] > 0.4:
+            print(f"Beat {current_beat}")
+
+    # Set what the current time in the song is
+    current_time = time.time() - start_of_song
